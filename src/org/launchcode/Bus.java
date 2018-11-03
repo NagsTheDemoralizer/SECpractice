bb6d162aa1b591463ffc16840fc35c18db4e0071
NagsTheProgrammer <34460480+NagsTheProgrammer@users.noreply.github.com>
2018-11-03 08:56:01 -0600

    updated Bus

--- src/org/launchcode/Bus.java
+++ src/org/launchcode/Bus.java
@@ -1,17 +1,20 @@
 package org.launchcode;
-import java.Arraylist;
+import java.util.ArrayList<E>;
 
 public class Bus {
 
     private int cost;
     private int capacity;
+    private ArrayList<BusStop> route = new ArrayList<BusStop>();
+    private int currentCap = 0;
 
     public Bus(){
         this.cost = 4;
-        this. capacity = 20;
+        this.capacity = 20;
+        this.route = null;
     }
 
-    public Bus(ArrayList<BusStop>){
-
+    public Bus(ArrayList<BusStop> busStops){
+        // change route
     }
 }
