b184aedd6b5cb9291af996aad91c2860f518c37e
NagsTheProgrammer <34460480+NagsTheProgrammer@users.noreply.github.com>
2018-11-03 09:03:36 -0600

    Update src/org/launchcode/Bus.java

--- src/org/launchcode/Bus.java
+++ src/org/launchcode/Bus.java
@@ -8,6 +8,9 @@ public class Bus {
     private ArrayList<BusStop> route = new ArrayList<BusStop>();
     private int currentCap = 0;
 
+    /*
+    default constructor for Bus class
+     */
     public Bus(){
         this.cost = 4;
         this.capacity = 20;
