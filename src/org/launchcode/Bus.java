394b03a3e096799e68d3a06389ea879146a93a7d
NagsTheProgrammer <34460480+NagsTheProgrammer@users.noreply.github.com>
2018-11-03 08:44:32 -0600

    Created Bus & BustStop classes

---
+++ src/org/launchcode/Bus.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+
+public class Bus {
+
+    private int cost;
+    private int capacity;
+
+    public Bus(){
+        this.cost = 4;
+        this. capacity = 20;
+    }
+}
