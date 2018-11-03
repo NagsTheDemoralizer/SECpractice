d692a1cd6095634e5db574c47b9f6c5a626463c4
Daine McNiven <daine.mcniven@hotmail.com>
2018-11-02 20:10:22 -0600

    Created a sample fxml file.

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -1,32 +1,45 @@
 package org.launchcode;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        //creating a Group object
-        Group group = new Group();
-
-        //Creating a Scene by passing the group object, height and width
-        Scene scene = new Scene(group ,600, 300);
-
-        //setting color to the scene
-        scene.setFill(Color.BROWN);
-
-        //Setting the title to Stage.
-        primaryStage.setTitle("Sample Application");
-
-        //Adding the scene to Stage
-        primaryStage.setScene(scene);
-
-        //Displaying the contents of the stage
+    public void start(Stage primaryStage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+//        //creating a Group object
+//        Group group = new Group();
+//
+//        //Creating a Scene by passing the group object, height and width
+//        Scene scene = new Scene(group ,600, 300);
+//
+//        //setting color to the scene
+//        scene.setFill(Color.BROWN);
+//
+//        //Setting the title to Stage.
+//        primaryStage.setTitle("Sample Application");
+//
+//        //Adding the scene to Stage
+//        primaryStage.setScene(scene);
+//
+//        //Displaying the contents of the stage
+//        primaryStage.show();
     }
     public static void main(String args[]){
         launch(args);
+    }
+
+    public void buttonPressed(ActionEvent e)
+    {
+        System.out.println("Button has been pressed");
     }
 }
